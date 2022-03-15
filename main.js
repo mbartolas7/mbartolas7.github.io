6519617a0d66fa6b33c7945456864fddebf872a7
Mathias6120 <93884797+Mathias6120@users.noreply.github.com>
2022-03-15 20:35:07 +0100

    Links

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 (function () {
-    const image_url = "https://scontent.fcrl2-1.fna.fbcdn.net/v/t39.30808-6/272112272_443075887496590_2154227924426512967_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=yLSHsSyvm20AX_oyqbM&_nc_oc=AQnS39_1H63HU0SoyXlpiDtna3hidj_SG31V5H7bQbqfy_UT8MyiVLtVWk00DICV-MlZwmA1Hi-GcU6URHpBlyJQ&_nc_ht=scontent.fcrl2-1.fna&oh=00_AT-mHknUfAfKja_JtICW3V8Rl8TkFwhArHVgwiKW4LQceA&oe=622B7B55"
+    const image_url = "https://fr.gravatar.com/userimage/218144972/4bae72ec0b32c8c5cdcc962019d81911.jpg?size=1000"
     const profileImage = document.getElementById("profileImage")
     profileImage.src = image_url    
-})(); 
+})();
