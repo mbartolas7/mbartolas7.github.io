b02c9bc97e455a278771b8558d7fee668cafb7b3
Mathias6120 <93884797+Mathias6120@users.noreply.github.com>
2022-09-18 18:16:50 +0200

    18/09

--- main.js
+++ main.js
@@ -2,4 +2,21 @@
     const image_url = "https://www.gravatar.com/avatar/a28cd32ac570bb3b1fe687d56193ba89?size=500"
     const profileImage = document.getElementById("profileImage")
     profileImage.src = image_url
+
+    fetch("https://api.unsplash.com/collections/_YyEkBmM_Ik/photos?client_id=VLrJYpYpyISvcyynCZYT1yMYh5V-Ki9YKZ0SBZq02zk").then(function(r) {
+        r.json().then(function(images) {
+            const imagesGrid = document.querySelector("#imagesGrid")
+
+            images.forEach(item => {
+                const newItem = document.createElement("li");
+                const newItem_img = document.createElement("img")
+                newItem.appendChild(newItem_img)
+        
+                newItem_img.classList.add("itemImage")
+                newItem_img.src = item.urls.regular
+                
+                imagesGrid.appendChild(newItem);
+            });
+        })
+    })
 })();
